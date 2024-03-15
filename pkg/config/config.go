6666b2b6dea4f89da0d77293c539e7b38db4ef3b
Martin Bozhilov <marogo142005@gmail.com>
2024-03-15 15:16:19 +0200

    [FIX] \n

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 type Config struct {
 	PinataApiKey       string `env:"PINATA_API_KEY"`
@@ -10,8 +13,8 @@ type Config struct {
 
 func LoadConfig() *Config {
 	return &Config{
-		PinataApiKey:       os.Getenv("PINATA_API_KEY"),
-		PinataSecretApiKey: os.Getenv("PINATA_SECRET_API_KEY"),
-		Port:               os.Getenv("PORT"),
+		PinataApiKey:       strings.TrimSpace(os.Getenv("PINATA_API_KEY")),
+		PinataSecretApiKey: strings.TrimSpace(os.Getenv("PINATA_SECRET_API_KEY")),
+		Port:               strings.TrimSpace(os.Getenv("PORT")),
 	}
 }
